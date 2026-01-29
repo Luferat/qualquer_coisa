5f09f30eb8cfd1f3b2dd0c71c95f7c9951385509
André Luferat <contato@luferat.net>
2026-01-28 22:44:11 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 // lib\main.dart
 
+// lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importe isso
+import 'firebase_options.dart'; // Arquivo gerado pelo FlutterFire CLI
 import 'package:qualquer_coisa/pages/home.dart';
 
-void main() {
+void main() async {
+  // Garante que os componentes do Flutter estejam prontos
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
